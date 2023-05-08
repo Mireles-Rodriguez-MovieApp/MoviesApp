9c836c6f0163e100daec70845ffa1c330c1b4ba8
Patrick Mireles <patrickm.mireles@gmail.com>
2023-05-08 16:51:08 -0500

    get data

--- js/movies.js
+++ js/movies.js
@@ -7,6 +7,15 @@
 
 
 
+// fetch("https://glitch.com/edit/#!/positive-half-anger?path=db.json%3A14%3A18")
+
+fetch("https://positive-half-anger.glitch.me/movies")
+    .then(response => response.json())
+    .then(movies => console.log(movies));
+
+
+
+
 
 
 
