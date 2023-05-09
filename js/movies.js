c2b05c9a9acaf954ffee27abf7f696bda0a79311
Patrick Mireles <patrickm.mireles@gmail.com>
2023-05-09 09:49:54 -0500

    added submit form, needs edits

--- js/movies.js
+++ js/movies.js
@@ -13,6 +13,26 @@ fetch("https://positive-half-anger.glitch.me/movies")
     .then(response => response.json())
     .then(movies => console.log(movies), $('#loading').html(''));
 
+const form = document.querySelector('form');
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault(); // prevent the default form submission behavior
+
+    // add director and genre here
+    const title = document.querySelector('#title').value;
+    const rating = document.querySelector('#rating').value;
+
+    // send the new movie data to the server
+    fetch('/api/movies', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ title, rating })
+    })
+        .then(response => response.json())
+        .then(data => console.log(data))
+        .catch(error => console.error(error));
+});
+
 
 
 
