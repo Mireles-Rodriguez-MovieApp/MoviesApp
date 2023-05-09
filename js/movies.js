2e701df6f6fb1e801ba1bbd634c9c19c9e50d138
Logan Rodriguez <Rodriguezlogan0428@gmail.com>
2023-05-09 09:52:30 -0500

    added cards

--- js/movies.js
+++ js/movies.js
@@ -11,7 +11,18 @@ $('#loading').html('<h1>Loading...</h1> <img src=\"assets/loading-circle.gif\">'
 
 fetch("https://positive-half-anger.glitch.me/movies")
     .then(response => response.json())
-    .then(movies => console.log(movies), $('#loading').html(''));
+    .then(movies => movies.forEach(movie => $('#cards').append("<div class=\"card\" style=\"width: 18rem;\">\n" +
+        // "  <img src=\"...\" class=\"card-img-top\" alt=\"...\">\n" +
+        "  <div class=\"card-body\">\n" +
+        "    <h5 class=\"card-title\"> "+ movie.title + "</h5>\n" +
+        "    <p class=\"card-text\">"+ movie.director +"</p>\n" +
+        "  </div>\n" +
+        "  <ul class=\"list-group list-group-flush\">\n" +
+        "    <li class=\"list-group-item\">Genre: "+ movie.genre +"</li>\n" +
+        "    <li class=\"list-group-item\">Rating: "+ movie.rating +"</li>\n" +
+        "  </ul>\n" +
+        "<a href=\"#\" class=\"btn btn-primary deleteButton\">Delete</a>"+
+        "</div>")), $('#loading').html(''))
 
 
 
